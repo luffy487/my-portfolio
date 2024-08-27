7312f1b7c92e5b9a6f9bd4a7c61fbb1729ac800e
vibhakar <vibhakar.vaddi@gmail.com>
2024-08-27 17:05:16 +0530

    db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const MONGO_STRING = process.env.MONGO_STRING;
+// const MONGO_STRING = process.env.MONGO_STRING;
 
-const mongoOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-};
+// const mongoOptions = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// };
 
-mongoose.Promise = global.Promise;
-mongoose
-  .connect(MONGO_STRING, mongoOptions)
-  .then(() => {
-    console.info("Successfully connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error while connecting to MongoDB:", err.message);
-  });
+// mongoose.Promise = global.Promise;
+// mongoose
+//   .connect(MONGO_STRING, mongoOptions)
+//   .then(() => {
+//     console.info("Successfully connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.error("Error while connecting to MongoDB:", err.message);
+//   });
